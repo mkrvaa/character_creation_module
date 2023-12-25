765aff94a6ce27ce1f20d760ddbcc11e419ca86f
Anastasia Makarova <153824565+mkrvaa@users.noreply.github.com>
2023-12-25 23:01:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from random import randint
 
 
 def attack(char_name, char_class):
-    """пу пу пу
-       char_namen -- пубу пубу
-       char_class -- бупубупу
+    """Пу пу пу.
+    Keyword arguments:
+    char_namen -- пубу пубу
+    char_class -- бупубупу.
     """
     if char_class == 'warrior':
         uron = 5 + randint(3, 5)
@@ -15,13 +16,14 @@ def attack(char_name, char_class):
     if char_class == 'healer':
         uron = 5 + randint(-3, -1)
         return (f'{char_name} нанёс урон противнику равный {uron}')
-    return ""
+    return ''
 
 
 def defence(char_name, char_class):
-    """пу пу пу      
-       char_namen -- пубу пубу
-       char_class -- бупубупу
+    """Пу пу пу.
+    Keyword arguments:
+    char_namen -- пубу пубу
+    char_class -- бупубупу
     """
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
@@ -29,28 +31,30 @@ def defence(char_name, char_class):
         return (f'{char_name} блокировал {10 + randint(-2, 2)} урона')
     if char_class == 'healer':
         return (f'{char_name} блокировал {10 + randint(2, 5)} урона')
-    return ""
+    return ''
 
 
-def special(char_name, char_class):   
-    """пу пу пу
-       char_namen -- пубу пубу
-       char_class -- бупубупу
+def special(char_name, char_class):
+    """Пу пу пу.
+    Keyword arguments:
+    char_namen -- пубу пубу
+    char_class -- бупубупу
     """
     if char_class == 'warrior':
-        u = 80 + 25
-        return (f'{char_name} применил специальное умение «Выносливость {u}»')
+        return (f'{char_name} применил специальное'
+                f'умение «Выносливость {80 + 25}»')
     if char_class == 'mage':
         return (f'{char_name} применил специальное умение «Атака {5 + 40}»')
     if char_class == 'healer':
         return (f'{char_name} применил специальное умение «Защита {10 + 30}»')
-    return ""
+    return ''
 
 
 def start_training(char_name, char_class):
-    """пу пу пу
-       char_namen -- пубу пубу
-       char_class -- бупубупу
+    """Пу пу пу.
+    Keyword arguments:
+    char_namen -- пубу пубу
+    char_class -- бупубупу
     """
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
@@ -59,9 +63,9 @@ def start_training(char_name, char_class):
     if char_class == 'healer':
         print(f'{char_name}, ты Лекарь — чародей, способный исцелять раны.')
     print('Потренируйся управлять своими навыками.')
-    print('Введи одну из команд: attack — чтобы атаковать противника,' +
-          ' defence — чтобы блокировать атаку противника или special —' +
-          ' чтобы использовать свою суперсилу.')
+    print('Введи одну из команд: attack — чтобы атаковать противника,'
+          'defence — чтобы блокировать атаку противника или special —'
+          'чтобы использовать свою суперсилу.')
     print('Если не хочешь тренироваться, введи команду skip.')
     cmd = ''
     while cmd != 'skip':
@@ -76,18 +80,19 @@ def start_training(char_name, char_class):
 
 
 def choice_char_class():
-    """пу пу пу
-       char_namen -- пубу пубу
-       char_class -- бупубупу
+    """Пу пу пу.
+    Keyword arguments:
+    char_namen -- пубу пубу
+    char_class -- бупубупу
     """
     approve_choice = None
     char_class = None
     while approve_choice != 'y':
         char_class = input('Введи название персонажа, за которого хочешь'
-                           + ' играть: Воитель — warrior, Маг — mage,'
-                           + ' Лекарь — healer: ')
+                           ' играть: Воитель — warrior, Маг — mage,'
+                           ' Лекарь — healer: ')
         if char_class == 'warrior':
-            print('Воитель — дерзкий воин ближнего боя.' +
+            print('Воитель — дерзкий воин ближнего боя.'
                   'Сильный, выносливый и отважный.')
         if char_class == 'mage':
             print('Маг — находчивый воин дальнего боя.' +
@@ -115,6 +120,6 @@ def main():
     print('Воитель, Маг, Лекарь')
     char_class = choice_char_class()
     print(start_training(char_name, char_class))
-
+    
 
 main()
